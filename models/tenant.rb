cb558297a8341694c82d64e3123e50983ced3d7b
Peter Zhang <pzhang87@gmail.com>
2015-11-03 16:51:44 -0500

    merge with solution

--- models/tenant.rb
+++ models/tenant.rb
@@ -1,13 +1,8 @@
-require 'pry'
-
-class Tenant
-  def initialize(name, age, gender)
-    @name = name
-    @age = age
-    @gender = gender
-  end
+class Tenant < ActiveRecord::Base
+  belongs_to :apartment
+  # def initialize(name, age, gender)
+  #   @name = name
+  #   @age = age
+  #   @gender = gender
+  # end
 end
-
-binding.pry
-
-puts "stfu"
